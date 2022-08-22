b6f2c13d9081b1d63994bb521fc01aa38f69c169
suku1221 <sukumarchokkapu@gmail.com>
2022-08-22 18:47:48 +0530

    my first commit

---
+++ src/test/java/Seleniumtest/Seleniumtest/maventest.java
@@ -0,0 +1,13 @@
+package Seleniumtest.Seleniumtest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class maventest {
+	
+	public  void testing() {
+		
+		WebDriver driver= new ChromeDriver();
+	}
+
+}
